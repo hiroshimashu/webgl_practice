db0f52eb32d0c229360a584ed208c2cc0db5aac5
Hiroshi Imamasu <hiroshimashu@gmail.com>
2018-05-21 15:20:55 +0900

    add add lines

--- homework2.js
+++ homework2.js
@@ -11,9 +11,16 @@
     let renderer;
     let geometry;
     let materialPoint;
-    let axesHelper;
+    let materialLine;
+    let materialCircle;
+    //let axesHelper;
+    let group;
     let particle;
     let point;
+    let line;
+    let circle;
+
+
 
     const RENDERER_PARAM = {
         clearColor: 0x333333
@@ -21,18 +28,25 @@
 
     const MATERIAL_PARAM_POINT = {
         color: 0x3399ff,
-        size: 0.1,
+        size: 0.7,
         sizeAttenuation: true
     };
 
+    const MATERIAL_LINE_PARAM = {
+        color: 0x3399ff
+    }
+
+    const MATERIAL_CIRCLE_PARAM = {
+        color: 0xffff00
+    }
 
     window.addEventListener('load', () => {
         canvasHeight = document.innerHeight;
         canvasWidth = document.innerWidth;
         targetDOM = document.getElementById('webgl');
-        console.log(targetDOM);
 
         scene = new THREE.Scene();
+        group = new THREE.Group();
         camera = new THREE.PerspectiveCamera(60, canvasWidth / canvasHeight, 0.1, 50.0);
         camera.position.x = 0;
         camera.position.y = 0;
@@ -46,17 +60,47 @@
         controls = new THREE.OrbitControls(camera, renderer.domElement);
 
         materialPoint = new THREE.PointsMaterial(MATERIAL_PARAM_POINT);
+        materialLine = new THREE.LineBasicMaterial(MATERIAL_LINE_PARAM);
+        materialCircle = new THREE.MeshBasicMaterial(MATERIAL_CIRCLE_PARAM);
+
+
+        // particle
         geometry = new THREE.Geometry();
-
-        point = new THREE.Vector3(0, 0, 0);
+        point = new THREE.Vector3(0, 0, 1);
         geometry.vertices.push(point);
-
         particle = new THREE.Points(geometry, materialPoint);
         scene.add(particle);
 
+        // circle
+        geometry = new THREE.CircleGeometry(3, 12);
+        circle = new THREE.Mesh( geometry, materialCircle );
+        scene.add(circle);
+
+
+
+        // axes1
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(
+            new THREE.Vector3( 0, 0, 1 ),
+            new THREE.Vector3( 0, 5, 1 )
+        )
+        line = new THREE.Line( geometry, materialLine );
+        group.add( line );
+
+
+        // axes2
+        geometry = new THREE.Geometry();
+        geometry.vertices.push(
+            new THREE.Vector3( 0, 0, 1 ),
+            new THREE.Vector3( 3, 0, 1 )
+        )
+        line = new THREE.Line( geometry, materialLine );
+        group.add( line );
+
+        scene.add( group );
         // helper
-        axesHelper = new THREE.AxesHelper(5.0);
-        scene.add(axesHelper);
+        //axesHelper = new THREE.AxesHelper(5.0);
+        //scene.add(axesHelper);
 
         window.addEventListener('resize', () => {
             renderer.setSize(window.innerWidth, window.innerHeight);
